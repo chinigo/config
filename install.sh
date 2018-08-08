7512621c8fc0a82b0746b65dd824c84410590c66
Michael Chinigo <chinigo@gmail.com>
2018-08-08 18:44:48 -0400

    Improve install script

--- install.sh
+++ install.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
+set -o errexit
+set -o nounset
 
-mv ~/.gitconfig ~/.gitconfig.bak
-ln -s `pwd`/gitconfig ~/.gitconfig
-touch ~/.gitconfig.local
+
+# Git
+[[ -e ~/.gitconfig ]] && mv -f ~/.gitconfig{,.bak}
+ln -fhs "$(pwd)/gitconfig" ~/.gitconfig
+[[ -e ~/.gitconfig.local ]] || touch ~/.gitconfig.local
+
+
+# ZSH
+[[ -e ~/.zshrc ]] && mv -f ~/.zshrc{,.bak}
+ln -fhs "$(pwd)/zshrc" ~/.zshrc
+
+[[ -e ~/.oh-my-zsh ]] && mv -f ~/.oh-my-zsh{,.bak}
+ln -fhs "$(pwd)/oh-my-zsh/" ~/.oh-my-zsh
+
+
+# Vim
+[[ -e ~/.vim ]] && mv -f ~/.vim{,.bak}
+ln -fhs "$(pwd)/vim-config/" ~/.vim
+
+vim +PluginInstall +qa
